4f4d4619894a2b0d01b6c7ca8aa375192d3d4b5c
Saadat Antor <76962594+Saadat-Antor@users.noreply.github.com>
2025-03-02 22:22:45 +0600

    Update step13-part_to_whole_analysis.sql

--- scripts/step13-part_to_whole_analysis.sql
+++ scripts/step13-part_to_whole_analysis.sql
@@ -1,1 +1,33 @@
+/*
+===============================================================================
+Part-to-Whole Analysis
+===============================================================================
+Purpose:
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
 
+SQL Functions Used:
+    - SUM(), AVG(): Aggregates values for comparison.
+    - Window Functions: SUM() OVER() for total calculations.
+===============================================================================
+*/
+
+-- determining categories that contribute the most to the overall sales
+WITH sales_by_category AS(
+SELECT
+p.category,
+SUM(s.sales_amount) AS total_sales
+FROM gold.fact_sales AS s
+LEFT JOIN gold.dim_products AS p
+ON s.product_key = p.product_key
+WHERE s.order_date IS NOT NULL
+GROUP BY p.category
+)
+SELECT
+category,
+total_sales,
+SUM(total_sales) OVER() AS overall_sales,
+CONCAT(ROUND((CAST(total_sales AS FLOAT)/SUM(total_sales) OVER())*100, 2), '%') AS percent_sales
+FROM sales_by_category
+ORDER BY total_sales DESC;
